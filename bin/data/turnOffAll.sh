ec4ca261c7c0028e789562dc20acc54ef97e2d23
PiMakers <PiMakers@gmail.com>
2019-06-19 10:04:55 +0200

    Simplified with one script

--- bin/data/turnOffAll.sh
+++ bin/data/turnOffAll.sh
@@ -1,74 +1,68 @@
 #!/bin/bash
-#net rpc shutdown -I IPADDRESS -U USERNAME%PASSWORD
-# set machine on the same network
 
+##===================================================
+## net rpc shutdown -I IPADDRESS -U USERNAME%PASSWORD
+## set machine on the same network
+## manage windowposition: sudo apt install wmctrl
+## notification error:sudo apt-get install libnotify-cil-dev
+##===================================================
+
+## TurnOff blankScreen:
+set s off
+xset -dpms
+xset s noblank
+
+ ## Arg 1 - Pid of window to move.
+ ## Arg 2 - X-Coord.
+ ## Arg 3 - Y-Coord.
+ ## E.g.  move_win $(pidof zenity) 0 0
+
+function move_win() { 
+ xdotool windowmove $(wmctrl -lp | grep ${1} | cut -d' ' -f1) ${2} ${3}
+} 
 
 command1() {
-## Víztorony 
-# 1. emelet
-IPADDRESS=192.168.137.111
-USERNAME=s1
+## Machines to shutdown 
+
 PASSWORD=sajt123
 RESTART="-r -t 0"
 
-if [ $(net rpc shutdown  -v ${RESTART} -I ${IPADDRESS} -U "${USERNAME}%${PASSWORD}" 2>/dev/null & ) ]; then
-    echo -ne "\r1.emelet lekapcsolva!\n"
-else
-    echo -ne "\r1.emelet sikertelen lekapcsolás\n"
-fi    
+
+for ((i=1;i<3;i++))
+    do
+        IPADDRESS=192.168.137.11${i}
+        USERNAME=s${i}
+
+
+        if [ $(net rpc shutdown  -v ${RESTART} -I ${IPADDRESS} -U "${USERNAME}%${PASSWORD}" 2>/dev/null & ) ]; then
+            c --text="${i}.emelet ( ${IPADDRESS} ) lekapcsolva!" --display=${DISPLAY} &
+        else
+            zenity --notification --text="${i}.emelet ( ${IPADDRESS} ) nem lett lekapcsolva!" --display=${DISPLAY} &
+        fi
+    done
 }
 
-
-command2() {
-# 2. emelet
-IPADDRESS=192.168.137.112
-USERNAME=s2
-PASSWORD=sajt123
-RESTART="-r -t 0"
-
-if [ $(sudo net rpc shutdown  -v ${RESTART} -I ${IPADDRESS} -U "${USERNAME}%${PASSWORD}" 2>/dev/null & ) ]; then
-    echo -ne "\r2.emelet lekapcsolva!\n"
-else
-    echo -ne "\r2.emelet sikertelen lekapcsolás\n"
-fi  
-
-#now
-#sudo shutdown 2>&1 &
-#    echo "A server lekapcsol!"
-
-#sleep 1
-
-#sudo shutdown -c
-
-}
 
 command3() {
 
-    if $(zenity --question --text="Hoolulu" --default-cancel) ; then
+    if $(zenity --question --text="Kikapcsolod a rendszert?" --title="Kikapcs" --default-cancel) ; then
     command1 &
-    command2 &
-    command4 &
+    shutdown &
+    if 
+        $((for ((i=0;i<100;i+=1))
+            do 
+                echo $i
+                sleep 0.6
+            done ) | \
+                zenity --progress --auto-close --text="A rendszer lekapcsol..." --title="+36 30 944 6153"--time-remaining --title=Shutdown) --display=${DISPLAY}
+        then echo "ok";
+    else
+        shutdown -c
+        command3
+    fi
     echo
     else command3
     fi
-}
-
-command4() {
-    TIME=$(err=$(shutdown 2>&1) && echo $err | sed  's/.*\(-[0-9][0-9] \)//; s/CEST.*//' && sudo shutdown -c)
-    H=${TIME%%:*}
-    M=${TIME%:*}
-    M=${M##*:}
-    S=${TIME##*:}
-    midnight=$(date -d 'today 00:00:00' '+%s')
-    SHUTDOWN_TIME_IN_SEC=$((3600*$H+60*$M+$S))
-    CURR_TIME_IN_SEC=$(( now - midnight ))
-    echo
-    while [[ $(($SHUTDOWN_TIME_IN_SEC - $CURR_TIME_IN_SEC)) > 0 ]]
-        do
-            now=$(date '+%s')
-            CURR_TIME_IN_SEC=$(( now - midnight ))
-            echo -ne "\r$(($SHUTDOWN_TIME_IN_SEC - $CURR_TIME_IN_SEC))"
-        done
 }
 
 command3
